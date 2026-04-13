878db3bfbbffd2eb79e2dad70208e31c67e55674
Limoncito1264 <armando.sanchez.limon@gmail.com>
2026-04-12 20:53:49 -0700

    configuracion de SFML

--- main.cpp
+++ main.cpp
@@ -1,14 +1,24 @@
 #include <iostream> 
+#include <SFML/Graphics.hpp>
 using namespace std;
 
-void funcionPrueba() {
-    cout << "Funcion prueba";
-}
 
 int main (){
-    cout << "hola\n";
-    cout << "Segunda prueba de enlace de git hub\n";
-    funcionPrueba();
-    system("pause");
+    sf:: RenderWindow window(sf::VideoMode({800, 600}), "Titulo");
+
+    while (window.isOpen()) 
+    {
+        while (auto event = window.pollEvent()) 
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color(64, 64, 64));
+        window.display();
+    }
+
     return 0;
 }
