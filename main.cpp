085408f68ce781ab4bf59960a171e6fc2d80fdf8
Limoncito1264 <armando.sanchez.limon@gmail.com>
2026-04-12 18:39:03 -0700

    tercera prueba de enlace

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream> 
 using namespace std;
 
+void funcionPrueba() {
+    cout << "Funcion prueba";
+}
+
 int main {
     cout << "hola\n";
     cout << "Segunda prueba de enlace de git hub\n";
+    funcionPrueba();
     return 0;
 }
