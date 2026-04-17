535a10882f47cc7c55648777dc413f7878499e5d
Limoncito1264 <armando.sanchez.limon@gmail.com>
2026-04-16 23:48:58 -0700

    se modifico la clase jugador para agregarle movimiento al personaje

--- main.cpp
+++ main.cpp
@@ -7,6 +7,102 @@
 #include <SFML/Network.hpp>
 using namespace std;
 
+
+class Jugador {
+protected:
+    string nombre;
+    int hp;
+    float velocidad;
+    sf::RectangleShape shape;
+
+    void initVariables() {
+        this->velocidad = 5.f;
+    }
+
+    void initShape() {
+        this->shape.setFillColor(sf::Color::Green);
+        this->shape.setSize(sf::Vector2f(50.f, 50.f));
+    }
+
+public:
+
+    Jugador(float x = 0.f, float y = 0.f) {
+        this->shape.setPosition({x, y});
+        this->initVariables();
+        this->initShape();
+    }
+
+
+    void updateInput() {
+
+        //imputs del teclado
+        //Izquierda
+        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+            this->shape.move({-this->velocidad, 0.f});
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+            this->shape.move({this->velocidad, 0.f});
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+            this->shape.move({0.f, -this->velocidad});
+        }
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+            this->shape.move({0.f, this->velocidad});
+        }
+
+    }
+
+    void updateWindowBoundsCollision(const sf::RenderTarget* target) {
+
+        float windowWidth = static_cast<float>(target->getSize(). x);
+        float windowHeight = static_cast<float>(target->getSize(). y);
+
+        //Izquierda
+        if(this->shape.getGlobalBounds().position.x <= 0.f)
+            this->shape.setPosition({0.f,this->shape.getGlobalBounds().position.y});
+        //Derecha
+        else if(this->shape.getGlobalBounds().position.x + this->shape.getGlobalBounds().size.x >= windowWidth)
+            this->shape.setPosition({target->getSize().x - this->shape.getGlobalBounds().size.x, this->shape.getGlobalBounds().position.y});
+
+        //Arriba
+        if(this->shape.getGlobalBounds().position.y <= 0.f)
+            this->shape.setPosition({this->shape.getGlobalBounds().position.x, 0.f});
+        //Abajo
+        else if(this->shape.getGlobalBounds().position.y + this->shape.getGlobalBounds().size.y >= windowHeight)
+            this->shape.setPosition({this->shape.getGlobalBounds().position.x, target->getSize().y - this->shape.getGlobalBounds().size.y});
+    }
+
+    void update(const sf::RenderTarget* target) {
+
+        this->updateInput();
+
+        //limites de la ventana (colision)
+        this->updateWindowBoundsCollision(target);
+
+    }
+
+    void render(sf::RenderTarget& target) {
+        target.draw(this->shape);
+    }
+
+    ~Jugador() {}
+};
+
+class knight : public Jugador {
+private:
+    
+public:
+   
+};
+
+class Mapa {
+private:
+    std::vector<sf::RectangleShape> tiles;
+public:
+
+};
+
+
 class Juego {
 private:
     //Variables
@@ -14,10 +110,13 @@ private:
     sf::RenderWindow window;
     sf::VideoMode videoMode;
 
+    Jugador jugador;
+
     //Funcion privada para inicializar la ventana
     void iniciarWindow() {
         videoMode.size = {800, 600};
         window.create(videoMode, "Videojuego", sf::Style::Titlebar | sf::Style::Close);
+        window.setFramerateLimit(60);
     }
 
 public:
@@ -51,14 +150,17 @@ public:
     void update() //Updatear frame
     {
         this->pollEvents();
+
+        this->jugador.update(&this->window);
     }
 
     void render() //Rendereizado del juego
     {
         //Limpieza del frame antiguo
-        this->window.clear(sf::Color(255, 0, 0, 255));
+        this->window.clear();
 
         //Dibujar juego
+        this->jugador.render(this->window);
 
         //Desplegar frame en el juego
         this->window.display();
@@ -69,48 +171,6 @@ public:
     ~Juego() {}
 
 };
-
-
-class Jugador {
-protected:
-    string nombre;
-    int hp;
-    float velocidad;
-    sf::RectangleShape body;
-    sf::Vector2f posicion;
-
-public:
-
-};
-
-class knight : public Jugador {
-private:
-    
-public:
-    void mover () {
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
-            posicion.y -= 1;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
-            posicion.x -= 1;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
-            posicion.y += 1;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
-            posicion.x += 1;
-        }
-    }
-};
-
-class Mapa {
-private:
-    std::vector<sf::RectangleShape> tiles;
-public:
-
-};
-
-
 
 int main (){
     //Ventana
