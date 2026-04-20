984b78f504dea2fd4117a9cdac6b7e8c0db051dc
Limoncito1264 <armando.sanchez.limon@gmail.com>
2026-04-20 01:52:19 -0700

    Agregacion de mapa, personaje, e interfaz del personaje

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream> 
+#include <memory>
+#include <vector>
+#include <map>
 
 #include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
 using namespace std;
@@ -13,41 +14,73 @@ protected:
     string nombre;
     int hp;
     float velocidad;
-    sf::RectangleShape shape;
+    sf::Texture texturaUp;
+    sf::Texture texturaDown;
+    sf::Texture texturaLeft;
+    sf::Texture texturaRight;
+    sf::Sprite sprite;
+
+    sf::Vector2f posicionAnterior;
 
     void initVariables() {
-        this->velocidad = 5.f;
+        this->velocidad = 10.f;
     }
 
-    void initShape() {
-        this->shape.setFillColor(sf::Color::Green);
-        this->shape.setSize(sf::Vector2f(50.f, 50.f));
+    float escalaBase = 0.2f;
+
+    void cambiarVista(const sf::Texture& nuevaTextura) {
+        this->sprite.setTexture(nuevaTextura, true);
+
+        sf::Vector2u sizeRef = this->texturaDown.getSize();
+        sf::Vector2u sizeNueva = nuevaTextura.getSize();
+
+        float factorX = (static_cast<float>(sizeRef.x) / sizeNueva.x) * escalaBase;
+        float factorY = (static_cast<float>(sizeRef.y) / sizeNueva.y) * escalaBase;
+
+        this->sprite.setScale({factorX, factorY});
     }
+
+
 
 public:
 
-    Jugador(float x = 0.f, float y = 0.f) {
-        this->shape.setPosition({x, y});
+    Jugador(float x = 0.f, float y = 0.f) : sprite(texturaDown) {
         this->initVariables();
-        this->initShape();
+
+        if(!this->texturaUp.loadFromFile("jugador_w.png") || !this->texturaDown.loadFromFile("jugador_s.png") || !this->texturaLeft.loadFromFile("jugador_a.png") || !this->texturaRight.loadFromFile("jugador_d.png")) {
+            cout << "Error cargando imagen " << endl;
+        }
+
+        this->cambiarVista(this->texturaDown);
+
+        this->sprite.setPosition(sf::Vector2f(x, y));
+
+    }
+
+    void regresarPosicion() {
+        sprite.setPosition(posicionAnterior);
     }
 
 
     void updateInput() {
-
+        posicionAnterior = sprite.getPosition();
         //imputs del teclado
         //Izquierda
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
-            this->shape.move({-this->velocidad, 0.f});
+            this->cambiarVista(this->texturaLeft);
+            this->sprite.move({-this->velocidad, 0.f});
         }
         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
-            this->shape.move({this->velocidad, 0.f});
+            this->cambiarVista(this->texturaRight);
+            this->sprite.move({this->velocidad, 0.f});
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
-            this->shape.move({0.f, -this->velocidad});
+            this->cambiarVista(this->texturaUp);
+            this->sprite.move({0.f, -this->velocidad});
         }
         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
-            this->shape.move({0.f, this->velocidad});
+            this->cambiarVista(this->texturaDown);
+            this->sprite.move({0.f, this->velocidad});
         }
 
     }
@@ -58,19 +91,24 @@ public:
         float windowHeight = static_cast<float>(target->getSize(). y);
 
         //Izquierda
-        if(this->shape.getGlobalBounds().position.x <= 0.f)
-            this->shape.setPosition({0.f,this->shape.getGlobalBounds().position.y});
+        if(this->sprite.getGlobalBounds().position.x <= 0.f)
+            this->sprite.setPosition({0.f,this->sprite.getGlobalBounds().position.y});
         //Derecha
-        else if(this->shape.getGlobalBounds().position.x + this->shape.getGlobalBounds().size.x >= windowWidth)
-            this->shape.setPosition({target->getSize().x - this->shape.getGlobalBounds().size.x, this->shape.getGlobalBounds().position.y});
+        else if(this->sprite.getGlobalBounds().position.x + this->sprite.getGlobalBounds().size.x >= windowWidth)
+            this->sprite.setPosition({target->getSize().x - this->sprite.getGlobalBounds().size.x, this->sprite.getGlobalBounds().position.y});
 
         //Arriba
-        if(this->shape.getGlobalBounds().position.y <= 0.f)
-            this->shape.setPosition({this->shape.getGlobalBounds().position.x, 0.f});
+        if(this->sprite.getGlobalBounds().position.y <= 0.f)
+            this->sprite.setPosition({this->sprite.getGlobalBounds().position.x, 0.f});
         //Abajo
-        else if(this->shape.getGlobalBounds().position.y + this->shape.getGlobalBounds().size.y >= windowHeight)
-            this->shape.setPosition({this->shape.getGlobalBounds().position.x, target->getSize().y - this->shape.getGlobalBounds().size.y});
+        else if(this->sprite.getGlobalBounds().position.y + this->sprite.getGlobalBounds().size.y >= windowHeight)
+            this->sprite.setPosition({this->sprite.getGlobalBounds().position.x, target->getSize().y - this->sprite.getGlobalBounds().size.y});
     }
+
+    sf::FloatRect getBounds() const {
+        return this->sprite.getGlobalBounds();
+    }
+
 
     void update(const sf::RenderTarget* target) {
 
@@ -82,10 +120,36 @@ public:
     }
 
     void render(sf::RenderTarget& target) {
-        target.draw(this->shape);
+        target.draw(this->sprite);
     }
 
     ~Jugador() {}
+};
+
+class Objeto {
+protected:
+    const sf::Texture& textura;
+    sf::Sprite sprite;
+
+public:
+
+    Objeto(const sf::Texture& textura, sf::Vector2f posicion) : textura(textura), sprite(textura) {
+        
+        this->sprite.setPosition(posicion);
+        this->sprite.setScale(sf::Vector2f(0.5f, 0.5f));
+
+    }
+
+    sf::FloatRect getBounds() const {
+        return sprite.getGlobalBounds();
+    }
+
+
+    void render(sf::RenderTarget& target) {
+        target.draw(sprite);
+    }
+
+
 };
 
 class knight : public Jugador {
@@ -110,19 +174,40 @@ private:
     sf::RenderWindow window;
     sf::VideoMode videoMode;
 
+    sf::Texture fondoTex;
+    sf::Sprite fondo;
+
+    map<string, sf::Texture> texturas;
+
     Jugador jugador;
+    vector<unique_ptr<Objeto>> objetos;
+
+
 
     //Funcion privada para inicializar la ventana
     void iniciarWindow() {
-        videoMode.size = {800, 600};
+        videoMode = sf::VideoMode({1280, 720});
         window.create(videoMode, "Videojuego", sf::Style::Titlebar | sf::Style::Close);
         window.setFramerateLimit(60);
     }
 
 public:
     //Constructores
-    Juego() {
+    Juego() : fondo(fondoTex) {
         this->iniciarWindow();
+
+        if (!this->fondoTex.loadFromFile("suelo.png")) {
+            cout << "Error cargando imagen " << endl;
+        } 
+
+        this->fondo.setTexture(this->fondoTex, true);
+
+        if (!this->texturas["estanteria"].loadFromFile("estanteria.png")) {
+            cout << "Error cargando estanteria " << endl;
+        }
+
+        this->objetos.push_back(std::make_unique<Objeto>(this->texturas["estanteria"], sf::Vector2f(700.f, 0.f)));
+        this->objetos.push_back(std::make_unique<Objeto>(this->texturas["estanteria"], sf::Vector2f(500.f, 0.f)));
     }
 
     //Funciones
@@ -152,6 +237,14 @@ public:
         this->pollEvents();
 
         this->jugador.update(&this->window);
+
+        for (auto& obj : objetos) {
+            if (jugador.getBounds().findIntersection(obj->getBounds())) {
+                jugador.regresarPosicion();
+            }
+        }
+
+
     }
 
     void render() //Rendereizado del juego
@@ -159,7 +252,13 @@ public:
         //Limpieza del frame antiguo
         this->window.clear();
 
+        //Dibujar fondo
+        this->window.draw(this->fondo);
+
         //Dibujar juego
+        for (auto& obj : objetos) {
+            obj->render(this->window);
+        }
         this->jugador.render(this->window);
 
         //Desplegar frame en el juego
